bd31d47c691a4181ee6dd239b225d4c2eaa20473
AHearn <ahearn1@emich.edu>
2026-05-04 23:28:33 -0400

    added setting for render

--- app.py
+++ app.py
@@ -18,11 +18,13 @@ from forms import TopicForm, LogForm,RegistrationForm, LoginForm
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
-db.init_app(app)
 
-with app.app_context():
-    db.create_all()
+db_url = os.getenv("DATABASE_URL")
+
+if db_url and db_url.startswith("postgres://"):
+    db_url = db_url.replace("postgres://", "postgresql://", 1)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = db_url
 
 #flask login
 login_manager = LoginManager()
